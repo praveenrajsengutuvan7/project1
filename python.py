998553442aadc84949a67b93ee7f0b97d54acb76
praveenraj2727 <160244019+praveenrajsengutuvan7@users.noreply.github.com>
2024-11-12 18:09:17 +0530

    Update python.py

--- python.py
+++ python.py
@@ -17,35 +17,28 @@ def quiz(y):
     a.add(frame4,text="Q4")
     a.add(frame5,text="Q5")
 
- Label(frame1,text="Encapsulation means?",font=("Segoe Script",50,)).grid(row=2,column=2)
+    Label(frame1,text="Encapsulation means?",font=("Segoe Script",50,)).grid(row=2,column=2)
     Button(frame1,text="Data hiding",font=("Segoe Script",30,"bold"),bg="light blue",command=a1_right).grid(row=3,column=1)
     Button(frame1,text="Data binding",font=("Segoe Script",30,"bold"),bg="light green",command=a1_wrong).grid(row=3,column=2)
     Button(frame1,text="Data abstraction",font=("Segoe Script",30,"bold"),bg="light pink",command=a1_wrong).grid(row=3,column=3)
 
     
-    Label(frame2,text="s='HarryStyles'print(s[0:3])",font=("Segoe Script",50,"bold")).grid(row=2,column=2)
+    Label(frame2,text="s='HarryStyles'print(s[1:3])",font=("Segoe Script",50,"bold")).grid(row=2,column=2)
     Button(frame2,text="Har",font=("Segoe Script",30,"bold"),bg="light blue",command=a2_wrong).grid(row=3,column=1)
     Button(frame2,text="arr",font=("Segoe Script",30,"bold"),bg="light green",command=a2_right).grid(row=3,column=2)
     Button(frame2,text="rry",font=("Segoe Script",30,"bold"),bg="light pink",command=a2_wrong).grid(row=3,column=3)
-  
-
-
-
- Label(frame3,text="Public,Private,Protected are?",font=("Segoe Script",50,"bold")).grid(row=2,column=2)
+    
+    Label(frame3,text="Public,Private,Protected are?",font=("Segoe Script",50,"bold")).grid(row=2,column=2)
     Button(frame3,text="Members",font=("Segoe Script",30,"bold"),bg="light blue",command=a3_right).grid(row=3,column=1)
     Button(frame3,text="Objects",font=("Segoe Script",30,"bold"),bg="light green",command=a3_wrong).grid(row=3,column=2)
     Button(frame3,text="Function",font=("Segoe Script",30,"bold"),bg="light pink",command=a3_wrong).grid(row=3,column=3)
  
     Label(frame4,text="Keyword use to declare a function",font=("Segoe Script",50,"bold")).grid(row=2,column=2)
-    Button(frame4,text="define",font=("Segoe Script",25,"bold"),bg="light blue",command=a4_wrong).grid(row=3,column=1)
-    Button(frame4,text="def",font=("Segoe Script",25,"bold"),bg="light green",command=a4_right).grid(row=3,column=2)
-    Button(frame4,text="None",font=("Segoe Script",25,"bold"),bg="light pink",command=a4_wrong).grid(row=3,column=3)
+    Button(frame4,text="define",font=("Segoe Script",30,"bold"),bg="light blue",command=a4_wrong).grid(row=3,column=1)
+    Button(frame4,text="def",font=("Segoe Script",30,"bold"),bg="light green",command=a4_right).grid(row=3,column=2)
+    Button(frame4,text="None",font=("Segoe Script",30,"bold"),bg="light pink",command=a4_wrong).grid(row=3,column=3)
 
- 
-
-
-
-   Label(frame5,text="Output of 2*12 ?",font=("Segoe Script",50,"bold")).grid(row=2,column=2)
+    Label(frame5,text="Output of 2*12 ?",font=("Segoe Script",50,"bold")).grid(row=2,column=2)
     Button(frame5,text="24",font=("Segoe Script",30,"bold"),bg="light blue",command=a5_right).grid(row=3,column=1)
     Button(frame5,text="28",font=("Segoe Script",30,"bold"),bg="light green",command=a5_wrong).grid(row=3,column=2)
     Button(frame5,text="32",font=("Segoe Script",30,"bold"),bg="light pink",command=a5_wrong).grid(row=3,column=3)
@@ -58,8 +51,6 @@ def a1_right():
 def a1_wrong():
     Label(frame1,text="Incorrect",font=("Segoe Script",40,"bold"),bg="red",fg="yellow").grid(row=1,column=2)
     Label(frame1,text="Marks Obtained = 0",font=("Calibri",40,"bold")).grid(row=4,column=2)
-
-
 
 def a2_right():
     Label(frame2,text="Correct",font=("Segoe Script",40,"bold"),fg="green").grid(row=1,column=2)
@@ -76,8 +67,6 @@ def a3_right():
 def a3_wrong():
     Label(frame3,text="Incorrect",font=("Segoe Script",40,"bold"),bg="red",fg="yellow").grid(row=1,column=2)
     Label(frame3,text="Marks Obtained = 0",font=("Calibri",40,"bold")).grid(row=4,column=2)
-
-
 
 def a4_right():
     Label(frame4,text="Correct",font=("Segoe Script",40,"bold"),fg="green").grid(row=1,column=2)
